544bf18caf066a0a027178af54f9b6ca86e9d430
潘卓然Y7000P <panzhuoran.parndeedlit@mapgis.com>
2020-09-28 14:23:58 +0800

    【SDK】【修复】【修复颜色色卡百分比越界问题】

--- src/map/style/color/interpolate.ts
+++ src/map/style/color/interpolate.ts
@@ -1,27 +1,29 @@
-import * as d3Scale from 'd3-scale';
-import * as d3Color from 'd3-color';
+import * as d3Scale from "d3-scale";
+import * as d3Color from "d3-color";
 
-import {
-    Categorical,
-    Diverging,
-    SequentialSingleHue, SequentialMultiHue,
-    Cyclical,
-    CategoricalList,
-    DivergingList,
-    SequentialSingleHueList, SequentialMultiHueList,
-    CyclicalList
-} from './colors';
-
+/**
+ * @description 颜色插值函数，专门用于遥感数据的颜色表建立
+ * @param {Number} [min = 0] 最小值
+ * @param {Number} [max = 255] 最大值
+ * @param {Number} [steps = 100]  分段数
+ * @param {Function} [interpolate] 插值函数，从Color.Interpolates里面获取对应的interpolate，这里不能是scheme
+ */
 export function interpolateStepsPercent(min, max, steps, interpolate) {
-    steps = steps || 255;
+  steps = steps || 255;
 
-    let format = "";
-    for (let i = 0; i <= steps; i++) {
-        const stemp = (Math.abs(max - min) / steps * i).toFixed(3);
-        const color = interpolate(stemp);
-        const colorStr = d3Color.color(color).hex().substring(1);
-        format += stemp + ":" + colorStr + "ff;";
-    }
-    format = format.substring(0, format.length - 1);
-    return format;
+  let format = "";
+  let stemp, percent;
+  let color, colorStr;
+  for (let i = 0; i <= steps; i++) {
+    stemp = ((Math.abs(max - min) / steps) * i).toFixed(3);
+    percent = (i / steps).toFixed(3);
+    color = interpolate(percent);
+    colorStr = d3Color
+      .color(color)
+      .hex()
+      .substring(1);
+    format += stemp + ":" + colorStr + "ff;";
+  }
+  format = format.substring(0, format.length - 1);
+  return format;
 }

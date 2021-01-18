8dd64801281ee571ec3f312c55af99ab1a3e7a2e
ParnDeedlit&潘卓然 <panzhuoran.parndeedlit@mapgis.com>
2021-01-18 17:05:26 +0800

    【SDK】【新增】【新增GeoJSON数据源的高亮实现&新增默认GeoJSON要素数据集】

--- src/map/format/geojson.ts
+++ src/map/format/geojson.ts
@@ -1,98 +1,110 @@
 import * as turf from "@turf/turf";
 import { MapRender } from "../document";
 
-export class GeojsonFeature {
-  private feature = undefined;
-  private bounds: any = undefined;
-
-  constructor() {}
-
-  createFeature(feature) {
-    this.feature = feature;
-  }
-
-  createGeomtry(geometry) {
-    this.feature = {
-      geometry: geometry,
-    };
-  }
-
-  createProperties(properties) {
-    this.feature = {
-      properties: properties,
-    };
-  }
-
-  getFeature() {
-    return this.feature;
-  }
-
-  getBounds(maprender?: MapRender | string) {
-    this.bbox();
-    const { bounds } = this;
-    if (maprender === MapRender.MapBoxGL || maprender === "mapboxgl") {
-      return [
-        [bounds[0], bounds[1]],
-        [bounds[2], bounds[3]],
-      ];
-    } else if (maprender === MapRender.Cesium || maprender === "cesium") {
+export let FeatureCollection =  {
+    type: 'geojson',
+    data: {
+        type: 'FeatureCollection',
+        features: []
     }
-    return bounds;
-  }
+};
 
-  bbox() {
-    this.bounds = turf.bbox(this.feature);
-    return this.bounds;
-  }
+export class GeojsonFeature {
+    private feature = undefined;
+    private bounds: any = undefined;
+
+    constructor() {
+        // constructor
+    }
+
+    createFeature(feature) {
+        this.feature = feature;
+    }
+
+    createGeomtry(geometry) {
+        this.feature = {
+            geometry
+        };
+    }
+
+    createProperties(properties) {
+        this.feature = {
+            properties
+        };
+    }
+
+    getFeature() {
+        return this.feature;
+    }
+
+    getBounds(maprender?: MapRender | string) {
+        this.bbox();
+        const { bounds } = this;
+        if (maprender === MapRender.MapBoxGL || maprender === "mapboxgl") {
+            return [
+                [bounds[0], bounds[1]],
+                [bounds[2], bounds[3]],
+            ];
+        } else if (maprender === MapRender.Cesium || maprender === "cesium") {
+            // cesium bounds
+        }
+        return bounds;
+    }
+
+    bbox() {
+        this.bounds = turf.bbox(this.feature);
+        return this.bounds;
+    }
 }
 
 export class GeojsonCollection {
-  private data = {
-    type: "FeatureCollection",
-    features: [],
-  };
+    public bounds: any = undefined;
 
-  public bounds: any = undefined;
+    private data = {
+        type: "FeatureCollection",
+        features: [],
+    };
 
-  constructor(features?: Array<GeojsonFeature> | Array<Object>) {
-    if (features instanceof Array) {
-      this.data.features = features || [];
-    } else {
-      this.data.features = [features];
+    constructor(features?: GeojsonFeature[] | object[]) {
+        if (features instanceof Array) {
+            this.data.features = features || [];
+        } else {
+            this.data.features = [features];
+        }
     }
-  }
 
-  addFeature(feature: GeojsonFeature | any) {
-    if (feature instanceof GeojsonFeature) {
-      this.data.features.push(feature.getFeature());
-    } else {
-      this.data.features.push(feature);
+    addFeature(feature: GeojsonFeature | any) {
+        if (feature instanceof GeojsonFeature) {
+            this.data.features.push(feature.getFeature());
+        } else {
+            this.data.features.push(feature);
+        }
     }
-  }
 
-  setFeatures(features) {
-    this.data.features = features;
-  }
-
-  getData() {
-    return this.data;
-  }
-
-  getBounds(maprender?: MapRender | string) {
-    this.bbox();
-    const { bounds } = this;
-    if (maprender === MapRender.MapBoxGL || maprender === "mapboxgl") {
-      return [
-        [bounds[0], bounds[1]],
-        [bounds[2], bounds[3]],
-      ];
-    } else if (maprender === MapRender.Cesium || maprender === "cesium") {
+    setFeatures(features) {
+        this.data.features = features;
     }
-    return bounds;
-  }
 
-  bbox() {
-    this.bounds = turf.bbox(this.data);
-    return this.bounds;
-  }
+    getData() {
+        return this.data;
+    }
+
+    getBounds(maprender?: MapRender | string) {
+        this.bbox();
+        const { bounds } = this;
+        if (maprender === MapRender.MapBoxGL || maprender === "mapboxgl") {
+            return [
+                [bounds[0], bounds[1]],
+                [bounds[2], bounds[3]],
+            ];
+        } else if (maprender === MapRender.Cesium || maprender === "cesium") {
+            // cesium bounds
+        }
+        return bounds;
+    }
+
+    bbox() {
+        this.bounds = turf.bbox(this.data);
+        return this.bounds;
+    }
 }
